a5ebbb36867d3e081f69af25ee7d774d06d39207
JanJon5000 <jan@ipoczta.pl>
2024-01-01 22:44:06 +0100

    bubble and selection sort algorithm

---
+++ sorting_algorithms.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+import numpy as np
+from pprint import pprint
+
+def selection_sort(l: list):
+    for i in range(len(l)):
+        swapper = min(l[i:])
+        l[l.index(swapper)] = l[i]
+        l[i] = swapper
+    return l
+
+def bubble_sort(l: list):
+    for i in range(len(l)):
+        flag = 0
+        for j in range(len(l)-i-1):
+            if l[j] > l[j+1]:
+                flag += 1
+                helper = l[j]
+                l[j] = l[j+1]
+                l[j+1] = helper
+        if flag == 0:
+            break
+    return l
+
+def insertion_sort(l: list):
+    pass
+
+def main():
+    l = [5132, 5645, 5144, 6682, 7708, 7710, 6689, 5160, 552, 1065, 44, 4142, 8751, 5168, 6711, 1592, 2622, 6735, 4691, 6743, 1121, 3170, 1124, 3173, 6775, 2680, 8828, 8317, 1664, 4737, 4738, 7817, 1162, 5263, 8847, 1180, 7326, 671, 9894, 5799, 680, 170, 175, 8881, 180, 9397, 1220, 197, 7366, 2759, 4820, 3812, 5863, 4328, 6380, 8429, 748, 9455, 7920, 1787, 5883, 6397, 4863, 3330, 6917, 1287, 4368, 1822, 3363, 5935, 5426, 4403, 9534, 3391, 6980, 6992, 7526, 1384, 7027, 4988, 6533, 1927, 392, 6025, 904, 8600, 5017, 4018, 4546, 965, 8652, 5071, 9167, 3539, 8153, 3035, 2025, 7663, 4600, 8188]
+    ypoints = np.array(bubble_sort(l))
+    plt.plot(ypoints, '.')
+    plt.show()
+    pprint(ypoints)
+
+if __name__=="__main__":
+    main()

3b7e0ccc64e79451ddb215e1d95af06fe08c6885
JanJon5000 <jan@ipoczta.pl>
2024-01-10 23:36:46 +0100

    affine cipher implemented

--- Algorithms/cipher_algorithms.py
+++ Algorithms/cipher_algorithms.py
@@ -12,11 +12,11 @@ def polibius_chessboard(string: str, dim: int) -> list:
     if currentString != '':
         ans.append(currentString)
     return ans
-def ADFGVX(password: str, alph: str, finalKeyWord: str) -> str:
+def ADFGVX(message: str, alph: str, finalKeyWord: str) -> str:
     alph = polibius_chessboard(alph, 6)
     absoluteKey = 'ADFGVX'
     encryptingString = ''
-    for char in ''.join([c for c in password.lower() if c != ' ']):
+    for char in ''.join([c for c in message.lower() if c != ' ']):
         indexes = []
         xChars = ''
         for i in range(len(alph)):
@@ -32,8 +32,22 @@ def ADFGVX(password: str, alph: str, finalKeyWord: str) -> str:
     encryptingString = {k:''.join([encryptingString[i][finalKeyWord.find(k)] for i in range(len(encryptingString))]) for k in sorted(finalKeyWord)}
     return (''.join(encryptingString.values()))
 
+def affine_cipher(K: tuple, message: str, alph: str) -> str:
+    alph = alph.upper()
+    message = message.upper()
+    val = lambda x: (K[0]*x + K[1])%len(alph)
+    ans = ''
+    for char in message:
+        if char in alph:
+            ans += alph[val(alph.index(char))]
+        else:
+            ans += char
+    return ans
+
 def main():
-    print(ADFGVX(password='attack at 1200am', alph='1q2w3e4r5t6y7u8i9o0pasdfghjklzxcvbnm', finalKeyWord='privacy'))
+    alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    print(affine_cipher((5, 8), 'A	F	F	I	N	E	C	I	P	H	E	R', alph))
+    pass
 
 if __name__=='__main__':
     main()

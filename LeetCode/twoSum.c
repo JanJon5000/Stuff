beaf776a494569fa39afb1857f63b2f5035a9a58
JanJon5000 <u58462@student.wat.edu.pl>
2025-10-02 22:52:42 +0200

    solved another leetcode problem and found longest substring with distinct characters in a string in C

--- LeetCode/twoSum.c
+++ LeetCode/twoSum.c
@@ -1,9 +1,52 @@
+// #include <stdio.h>
+// #include <math.h>
+// struct node{
+//     int value, key;
+//     struct node* next;
+// };
+
+// void setNode(struct node* n, int key,  int value){
+//     n->value = value;
+//     n->key = key;
+//     n->next = NULL;
+// }
+
+// struct hashMap {
+//     int capacity;
+//     struct node** arr;
+// };
+
+// struct hashMap* initializeHashMap(int capacity){
+//     struct hashMap *ans = malloc(sizeof(struct hashMap));
+//     ans->capacity = capacity;
+//     ans->arr = malloc(capacity * sizeof(struct node));
+//     return ans;
+// }
+
+// void insert(struct hashMap* map, struct node* n, int target){
+//     struct node* placeholder = *((map->arr)+(abs(target-n->value)%target));
+//     while(placeholder->next != NULL){
+//         placeholder = placeholder->next;
+//     }
+//     placeholder->next = n;
+// }
+
+// struct node* get(int value, struct hashMap* map, int target, struct node* n){
+//     return *((map->arr)+(abs(target-n->value)%target));
+// }
+
+// int* twoSum(int* nums, int numSize, int target, int* returnSize){
+//     int* ans = malloc(sizeof(int));
+//     struct hashMap h* = initializeHashMap(target-1);
+//     for(int i=0;i<numSize;i++){
+//         if(get(nums[i], h, target, ))
+//     }
+//     return ans;
+// }
 int* twoSum(int* nums, int numSize, int target, int* returnSize){
     int* ans = malloc(2 * sizeof(int));
     *returnSize = 2;
     for(int i=0;i<numSize-1;i++){
-        if(nums[i]>target)
-            continue;
         for(int j=i+1;j<numSize;j++){
             if(nums[i] + nums[j] == target){
                 ans[0] = i; ans[1] = j;
